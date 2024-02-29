078802a32bcda6f513fd7eb7992b24246141f5a5
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-02-29 20:24:22 +0000

    Add model for landing page

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class HomePage(models.Model):
+    introduction = models.TextField()
+
+    def __str__(self):
+        return "Context for Landing Page"
