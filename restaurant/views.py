bf86ef772ab51252fc17905f0674ed657edf1b95
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-02-29 21:02:15 +0000

    Add view for HomePage and update urls. Add templates folder to 'restaurant' app to add home.html

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import HomePage
+
 
 # Create your views here.
+
+def home(request):
+    content_home = HomePage.objects.all()
+
+    context = {'home': home,}
+
+    return render(request, 'home/home.html', context)
