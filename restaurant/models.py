979ea7708c71d816e73da1acd1ee4332974da912
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-03-08 17:02:22 +0000

    Add models and views for foodmenu

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,3 +10,19 @@ class HomePage(models.Model):
 
     def __str__(self):
         return "Content for Landing Page"
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class MenuItems(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import HomePage
+from .models import HomePage, Category, MenuItems
 
 
 # Create your views here.
@@ -10,3 +10,12 @@ def home(request):
     context = {'content_home': content_home}
 
     return render(request, 'index.html', context)
+
+
+def foodmenu(request):
+    categories = Category.objects.all()
+    menu_items = MenuItems.objects.all()
+    context = {'categories': categories, 'menu_items': menu_items}
+    return render(request, 'menu.html', context)
+
+
