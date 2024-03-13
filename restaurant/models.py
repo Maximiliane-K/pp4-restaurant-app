1f7ea0e912c85a214c5b45a9cc1c0396dc9d4761
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-03-13 14:48:57 +0000

    Add model for beverages menu

--- restaurant/models.py
+++ restaurant/models.py
@@ -40,3 +40,32 @@ class MenuItems(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class BeveragesCategory(models.Model):
+    """
+    Model for drink categories in menu
+    """
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Drink Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class BeveregeItems(models.Model):
+    """
+    Model for beverage items
+    """
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(DrinkCategoryCategory,related_name='beverege_items', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Beverage Items'
+
+    def __str__(self):
+        return self.name
+
