60d9e83dbfdc5a8a700e22a3cc88c21594ddb502
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-03-13 16:19:27 +0000

    Register BeverageCategory and BeverageItem models in admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import HomePage, Category, MenuItems
+from .models import HomePage, Category, MenuItems, BeverageCategory, BeverageItems
 
 
 admin.site.register(HomePage)
 admin.site.register(Category)
 admin.site.register(MenuItems)
+admin.site.register(BeverageCategory)
+admin.site.register(BeverageItems)
