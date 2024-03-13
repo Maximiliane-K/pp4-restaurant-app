c3a8f52c21bb8ab339fc30244e0fa22c2f308cc6
Maximiliane <maximiliane.kaempffer@gmail.com>
2024-03-13 15:03:59 +0000

    Reafctor model for beverages

--- restaurant/models.py
+++ restaurant/models.py
@@ -42,26 +42,26 @@ class MenuItems(models.Model):
         return self.name
 
 
-class BeveragesCategory(models.Model):
+class BeverageCategory(models.Model):
     """
     Model for drink categories in menu
     """
     name = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name_plural = 'Drink Categories'
+        verbose_name_plural = 'Beverage Categories'
 
     def __str__(self):
         return self.name
 
 
-class BeveregeItems(models.Model):
+class BeverageItems(models.Model):
     """
     Model for beverage items
     """
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(DrinkCategoryCategory,related_name='beverege_items', on_delete=models.CASCADE)
+    beverage_category = models.ForeignKey(BeverageCategory,related_name='beverage_items', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Beverage Items'
